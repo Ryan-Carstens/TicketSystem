a4fa427765bc634d80e48d1ac3cd2430590cbff0
noname <email@email.com>
2015-09-28 17:30:00 +0200

    Signup Service Update

--- src/main/java/za/ac/rcarstens/assignment6_2/api/SignUpPage.java
+++ src/main/java/za/ac/rcarstens/assignment6_2/api/SignUpPage.java
@@ -1,14 +1,13 @@
 package za.ac.rcarstens.assignment6_2.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
+import za.ac.rcarstens.assignment6_2.domain.Admin;
 import za.ac.rcarstens.assignment6_2.domain.Customer;
 import za.ac.rcarstens.assignment6_2.services.AdminServices;
 import za.ac.rcarstens.assignment6_2.services.CustomerServices;
@@ -29,7 +28,7 @@ public class SignUpPage {
     //-------------------Create a Customer--------------------------------------------------------
 
     @RequestMapping(value = "/customer/signup", method = RequestMethod.POST)
-    public ResponseEntity<Void> createCustomer(@RequestBody Customer customer,    UriComponentsBuilder ucBuilder) {
+    public ResponseEntity<Void> createCustomer(@RequestBody Customer customer) {
         System.out.println("Creating Customer " + customer.getFullNameDeats().getFirstname());
 
 //     USE THIS IF YOU WANT TO CHECK UNIQUE OBJECT
@@ -40,9 +39,24 @@ public class SignUpPage {
 
         service1.save(customer);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ucBuilder.path("/customer/{id}").buildAndExpand(customer.getID()).toUri());
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+
+    //-------------------Create a Customer--------------------------------------------------------
+
+    @RequestMapping(value = "/admin/signup", method = RequestMethod.POST)
+    public ResponseEntity<Void> createAdmin(@RequestBody Admin admin) {
+        System.out.println("Creating admin " + admin.getFullNameDeats().getFirstname());
+
+//     USE THIS IF YOU WANT TO CHECK UNIQUE OBJECT
+        if (service1.exists(admin.getID())) {
+            System.out.println("A Subject with name " + admin.getFullNameDeats().getFirstname() + " already exist");
+            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+        }
+
+        service2.save(admin);
+
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
 }
